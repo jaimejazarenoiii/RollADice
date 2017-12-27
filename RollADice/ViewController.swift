1ade7af84d6243e2554ed48bcb98e6eb700b6a51
Jaime Jazareno III <jaimejazarenoiii@gmail.com>
2017-12-27 22:35:49 +0800

    Initial finished files

--- RollADice/ViewController.swift
+++ RollADice/ViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var label: UILabel!
+    @IBOutlet weak var imageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +23,15 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func rollDice(_ sender: Any) {
+        let min : UInt32 = 1
+        let max : UInt32 = 7
+        
+        let randomNumber = arc4random_uniform(max - min) + min
+        label.text = String("Rolled a \(randomNumber)")
+        imageView.image = UIImage(named: "Dice\(randomNumber)")
+        
+    }
+    
 }
 
